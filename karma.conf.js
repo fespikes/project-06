af342c07adb29a0f40ffbbedd641ec58ea81738c
rockwang <rockwang@localhost.localdomain>
2019-05-07 15:52:06 +0800

    ut repare

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['google-chrome'],
+    browsers: ['Chrome'],
     browserNoActivityTimeout: 300000,
     retryLimit: 5,
     singleRun: false
