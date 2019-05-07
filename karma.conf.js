27665ee3bca5b8a68ed18707b5ba3af1c78c821a
rockwang <rockwang@localhost.localdomain>
2019-05-07 15:47:36 +0800

    docker related

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['google-chrome'],
     browserNoActivityTimeout: 300000,
     retryLimit: 5,
     singleRun: false
